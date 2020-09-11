56803a7088072249f7b44c4efd5a601ebd2f9670
Alejandro Sivila <a2019116316@estudiantes.upsa.edu.bo>
2020-09-11 19:20:03 -0400

    se creo el controlador con autorizaciones (student)

---
+++ ApiSecureStudent/ApiSecureStudent/Models/DattaContext.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ApiSecureStudent.Models
+{
+    using System.Data.Entity;
+    public class DattaContext : DbContext 
+    {
+        public DattaContext(): base("DefaultConnection") //ctor +tab+tab
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ApiSecureStudent.Models.Student> Students { get; set; }
+    }
+}
---
+++ ApiSecureStudent/ApiSecureStudent/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿namespace ApiSecureStudent.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Student
+    {
+        [Key]
+        [Range(1, 9999)]
+        public int StudentId { get; set; }//prop + tab +tap
+
+        [StringLength(30)]
+        [Required(ErrorMessage = "YOU MUST ENTER THE FIELD {0}")]
+        public string LastName { get; set; }
+        [StringLength(30, ErrorMessage = "THE FIELD {0} MUST CONTAIN BETWEN {2} AND {1} CHARACTERS", MinimumLength = 2)] //EL 1 ES PARA EL EL 30 EL 2  PARA LA SEGUNDA CONDICION Y EL 0 PARA, EN ESTE CASO EL "FIRST NAME""
+        [Required(ErrorMessage = "YOU MUST ENTER THE FIELD {0}")]
+        public string FirstName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/mm/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
